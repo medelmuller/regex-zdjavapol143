b12cc7eded23d7991687836175065785375befd2
medelmuller <m.edelmuller@gmail.com>
2023-09-26 18:45:43 +0200

    Added Regex001 changes

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -2,9 +2,13 @@ package regex;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/*
+Napisz Regex, który sprawdza polskie numery telefonów komórkowych,
+zaczynające się cyfrą 5.
+Uwzględniaj że na początku może ale nie musi wystąpić +48
+ */
 public class Regex001 {
-    private static final Pattern PATTERN = Pattern.compile("5[0-9]{8}");
+    private static final Pattern PATTERN = Pattern.compile("(\\+48)?5[0-9]{8}");
 
     public static void main(String[] args) {
         /*
@@ -16,6 +20,8 @@ public class Regex001 {
         Fałsz dla tekstu: 712123
         Fałsz dla tekstu: 51123
          */
+        validate("+48511221236"); //true
+        validate("+48715123456"); //false
         validate("515332123");
         validate("515336293");
         validate("415336293");
